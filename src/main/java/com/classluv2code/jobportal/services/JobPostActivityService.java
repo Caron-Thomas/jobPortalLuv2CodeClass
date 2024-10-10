2dc35ab377d3fe4674a2dcbc42b91edd33df0b81
Caron-Thomas <caron.thomas@gmail.com>
2024-10-10 17:44:37 -0300

    add recuiter dashboard

---
+++ src/main/java/com/classluv2code/jobportal/services/JobPostActivityService.java
@@ -0,0 +1,39 @@
+package com.classluv2code.jobportal.services;
+
+import com.classluv2code.jobportal.entity.*;
+import com.classluv2code.jobportal.repository.JobPostActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobPostActivityService {
+
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    @Autowired
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+
+        List<IRecruiterJobs> recruiterJobs = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtos = new ArrayList<>();
+
+        for(IRecruiterJobs rec: recruiterJobs) {
+            JobLocation jobLocation = new JobLocation(rec.getLocationId(), rec.getCity(),rec.getState(),rec.getCountry());
+            JobCompany jobCompany = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtos.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id()
+                    ,rec.getJob_title(), jobLocation, jobCompany));
+        }
+        return recruiterJobsDtos;
+    }
+}
